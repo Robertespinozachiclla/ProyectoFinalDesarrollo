8287853375d8a415d48f71fa9c7d66cda6e80973
Robert Espinoza Chiclla <231174@unamba.edu.pe>
2026-06-29 21:36:51 -0500

    Implementacion del servicio de autenticacion

---
+++ src/main/java/epiis/unamba/Security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package epiis.unamba.Security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import epiis.unamba.Model.Usuario;
+import epiis.unamba.Repository.UsuarioRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Usuario no encontrado"));
+
+        return User.builder()
+                .username(usuario.getUsername())
+                .password(usuario.getPassword())
+                .roles(usuario.getRol().name())
+                .build();
+    }
+}
